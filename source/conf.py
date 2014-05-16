f5a9a198eca1b4d75a975352a5ab0d791414c6df
Ujaval Gandhi <ujaval@gmail.com>
2014-05-16 14:35:37 +0530

    logo for pdf files

--- source/conf.py
+++ source/conf.py
@@ -120,7 +120,7 @@ html_title = 'QGIS Tutorials and Tips'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../resources/en/logo.png'
+html_logo = '../resources/en/docs/common/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
